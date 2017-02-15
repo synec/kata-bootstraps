e4489f09187ebd709e162f82831580c3b82f3715
Daniel Grabert <dg@indivirtuell.net>
2017-02-15 13:54:18 +0100

    FizzBazz kata from http://kata-log.rocks/fizz-buzz-kata

--- js/jasmine/spec/HelloWorldSpec.js
+++ js/jasmine/spec/HelloWorldSpec.js
@@ -1,5 +1,27 @@
-describe("Hello world", function() {
-  it("says hello", function() {
-    expect(helloWorld()).toEqual("Hello!");
+describe("fizzBazz()", function() {
+
+  it("returns value if not dividable by 3 or 5", function() {
+    expect(fizzBazz(1)).toEqual(1);
+    expect(fizzBazz(2)).toEqual(2);
+  });
+
+  it("returns 'Fizz' for values dividable by 3", function() {
+    expect(fizzBazz(3)).toEqual('Fizz');
+  });
+
+  it("returns 'Bazz' for values dividable by 5", function() {
+    expect(fizzBazz(5)).toEqual('Bazz');
+  });
+
+  it("returns 'FizzBazz' for values dividable by 5 and 3", function() {
+    expect(fizzBazz(15)).toEqual('FizzBazz');
+  });
+});
+
+describe('fizzBazz100()', () => {
+  it('return the fizzBazz for 100 entries', () => {
+    const result = fizzBazz100();
+    expect(result.length).toEqual(100);
+    expect(result).toEqual(jasmine.arrayContaining([1,2,'Fizz',4,'Bazz','Fizz',7,8,'Fizz', 'Bazz', 11, 'Fizz', 13, 14, 'FizzBazz']));
   });
 });
--- js/jasmine/src/HelloWorld.js
+++ js/jasmine/src/HelloWorld.js
@@ -2,3 +2,15 @@ function helloWorld() {
   return "Hello world!";
 }
 
+function fizzBazz(value) {
+  if (value % 3 === 0 && value % 5 === 0) return 'FizzBazz';
+  if (value % 3 === 0) return 'Fizz';
+  if (value % 5 === 0) return 'Bazz';
+  return value;
+}
+
+function fizzBazz100() {
+  return Array(100)
+      .fill('')
+      .map((item, index) => fizzBazz(index + 1));
+}
